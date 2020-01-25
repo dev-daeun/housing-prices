7d4370231f087b63ac806a2973cafb5decd5b981
kde6260 <kde6260@gmail.com>
2020-01-25 16:18:28 +0900

    tag & push an image to dockerhub repo

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
-
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=kde6260/housing-prices:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag housing-prices $dockerpath
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
