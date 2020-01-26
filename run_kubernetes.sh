02c5878757ba3a72d9c2c8e4d6b3b5825f0883cb
kde6260 <kde6260@gmail.com>
2020-01-27 00:02:09 +0900

    rewrite run_kubernetes.sh to create pod & foward pod to the host

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+DOCKER_IMAGE_PATH=kde6260/housing-prices:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment housing-prices --image=$DOCKER_IMAGE_PATH
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods | grep housing-prices
+POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $POD_NAME 8000:80
